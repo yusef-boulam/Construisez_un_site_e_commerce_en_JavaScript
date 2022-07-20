e3566eeae52cbd8ccfca93eeae9af0f7e84c627f
yusef <yusefdev@outlook.fr>
2022-07-20 10:35:25 +0200

    ajout des produits dans le panier + redirection de la page produit un fois les produits ajoutés

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,4 @@
 
-//FONCTION qui charge le tableau depuis le local storage /////////////////////////////////////////////////////////////////////
-const chargeArrayPanier = () => {
-    const arrayPanier = JSON.parse(localStorage.arrayPanier);
-    return arrayPanier
-  }
-
-
 //FONCTION remplit le panier ////////////////////////////////////////////////////////////
   const showPanier = async () => {
    // on initialise la variable
@@ -19,43 +12,24 @@ const chargeArrayPanier = () => {
         let datas = await getCanap('http://localhost:3000/api/products');
 
 
-    // /* on boucle avec la METHODE forEach sur chaque objet du datas 
-    //    et on charge les valeurs dans canapCards  */
-    //    arrayPanier.forEach(objet => {
-    //     const objet = {
-    //       constructor(idProduit, couleurProduit, quantiteProduit) {
-    //         this.idProduit = idProduit; 
-    //         this.idProduit = idProduit; 
-    //         this.idProduit = idProduit; 
-    //       }
-    //     }
-       
-       
-    //     const idProduit  = objet.idProduit 
-    //     const couleurProduit  = objet.couleurProduit
-    //     const quantiteProduit = objet.quantiteProduit
-
-    //    });
-
-
-
-
-
+/* on boucle avec la METHODE forEach sur chaque objet du localStorage 
+      et on charge les objets dans le panier  */
        arrayPanier.forEach(objet => {
-     ` <article class="cart__item" data-id= ${idProduit} data-color=${couleurProduit} >
+
+     objetPanier +=` <article class="cart__item" data-id= ${objet._id} data-color=${objet.color} >
        <div class="cart__item__img">
-         <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+         <img src=${objet.image} alt="Photographie d'un canapé">
        </div>
        <div class="cart__item__content">
          <div class="cart__item__content__description">
-           <h2>Nom du produit</h2>
-           <p>Vert</p>
+           <h2>${objet.name}</h2>
+           <p>${objet.color}</p>
            <p>42,00 €</p>
          </div>
          <div class="cart__item__content__settings">
            <div class="cart__item__content__settings__quantity">
              <p>Qté : </p>
-             <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+             <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${objet.quantity}>
            </div>
            <div class="cart__item__content__settings__delete">
              <p class="deleteItem">Supprimer</p>
--- js/product.js
+++ js/product.js
@@ -1,14 +1,13 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //INSERER LES DETAILS CANAPE SELECTIONNE DANS LA PAGE
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // on crée la FONCTION qui recupere l'ID dans l'URL  ////////////////////////////////////////////////////////////////////////
 const recupId = () => {
   //on recupere url du produit
   const url = new URL(document.location.href);
   // on recupere l'id dans l'Url
   const idProduit = url.searchParams.get("id");
-  return idProduit
+  return idProduit;
 }
 
 ///on crée la FONCTION qui récupère la CARD CANAPE dans la base de donnée et l'ijecte dans le HTML//////////////////////////////
@@ -22,32 +21,26 @@ const showListProduct = async () => {
   console.log(datas)
 
   // on injecte les datas dans le code HTML
-  document.querySelector(".item__img").innerHTML = `<img src=${datas.imageUrl} alt="Photographie d'un canapé">`
+  document.querySelector(".item__img").innerHTML = `<img src=${datas.imageUrl} alt="Photographie d'un canapé">`;
   document.querySelector("#title").innerHTML = `${datas.name}`;
-  document.querySelector("#price").innerHTML = `${datas.price}`
-  document.querySelector("#description").innerHTML = `${datas.description}`
+  document.querySelector("#price").innerHTML = `${datas.price}`;
+  document.querySelector("#description").innerHTML = `${datas.description}`;
 
   // on injecte les couleurs séléctionnables dans le code HTML avec +=
   for (color of datas.colors) {
-    const addOptionCouleur = `<option value="${color}">${color}</option>`
-    document.querySelector("#colors").innerHTML += addOptionCouleur
+    const addOptionCouleur = `<option value="${color}">${color}</option>`;
+    document.querySelector("#colors").innerHTML += addOptionCouleur;
   };
 
   // on execute la FONCTION///////////////////////////////////////////////////////////////////////////////////////
 AjoutPanier(datas);
-}
+};
 
 // on execute la FONCTION///////////////////////////////////////////////////////////////////////////////////////
 showListProduct();
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//AJOUT PANIER
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//FONCTION (MAIN) /////////////////////////////////////////////////////////////////////////////////////////////////
+//FONCTION AJOUT PANIER /////////////////////////////////////////////////////////////////////////////////////////////////
 const AjoutPanier = (datas) => {
 
   // on initialise le tableau si localStorage vide sinon on charge le localStorage
@@ -63,15 +56,14 @@ const AjoutPanier = (datas) => {
           quantity: Number(document.getElementById("quantity").value),
           image : datas.imageUrl ,
           name: datas.name,
-          description: datas.description,
-        }
+        };
         console.log("newObjetPanier", newObjetPanier)
 
       // on verifie que la quantite et la couleur soit saisie
       if ((newObjetPanier.quantity === 0) || (newObjetPanier.color === 0)) {
         alert("il manque la quantité ou la couleur du produit");
         return;
-      }
+      };
         // on verifie si le produit à deja été ajouté 
         const produitIdentique =  arrayPanier.find(element => element._id === newObjetPanier._id && element.color === newObjetPanier.color);
         console.log(produitIdentique)
@@ -87,7 +79,10 @@ const AjoutPanier = (datas) => {
         }
         sauvegardeLocalStorage(arrayPanier);
         console.log("localStorage", localStorage)
+
+                // redirection vers le panier
+        location.href= "./cart.html"
       }
     )
-}
+};
 
