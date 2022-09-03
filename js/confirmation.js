c7fb17219f2d31cf52fa6b41aab3d77208389961
yusef <yusefdev@outlook.fr>
2022-09-03 15:20:41 +0200

    si aucun numero de commande renvoi sur la hoem page

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,11 +1,17 @@
 
 //on crée une fonction qui recupere le numero de commande présent dans l'URL et on l'insere dans la page HTML
-
 const showOrderId = () => {
     //on recupere url du produit
     const url = new URL(document.location.href);
     // on recupere l'id dans l'Url
     const orderid = url.searchParams.get("orderid");
+    console.log(orderid)
+     // si local storage vide on retourne sur la home page
+     if (orderid === null) {
+      alert("aucun numéro de commande présent");
+      location.href = "./index.html"
+      return
+    }
      // on l'insere dans le HTML
   document.getElementById("orderId").innerHTML= orderid;
      // on vide le panier
